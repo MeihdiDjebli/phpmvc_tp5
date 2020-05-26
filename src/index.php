1215dce09b3d9cd756d9f241e2c9019cc6c59117
Meihdi Djebli <meihdi.djebli@cnam.fr>
2020-05-26 03:07:15 +0200

    Création TP 5.1

---
+++ src/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Feed Animals</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- See https://fontawesome.com/v4.7.0/icons/ for more informations -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossorigin="anonymous"
+    >
+
+</head>
+
+<body>
+
+<div class="container">
+    <h1>Bon courage !</h1>
+</div>
+
+<!-- Bootstrap core JavaScript -->
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
